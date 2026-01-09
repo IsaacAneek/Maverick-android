0ecbc3621542b827cc1eb60003649055286e86d2
Rana094 <81284474+IsaacAneek@users.noreply.github.com>
2026-01-09 11:34:20 +0600

    Added kanban and pomodoro fragments

--- app/src/main/java/com/example/maverick/MainActivity.java
+++ app/src/main/java/com/example/maverick/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.maverick;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView side_bar;
@@ -44,5 +48,27 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
+        side_bar.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.kanban_board) {
+            KanbanBoardFragment kanbanBoardFragment = new KanbanBoardFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, kanbanBoardFragment).commit();
+        }
+        else if(id == R.id.pomodoro) {
+            PomodoroFragment pomodoroFragment = new PomodoroFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, pomodoroFragment).commit();
+            side_bar.setCheckedItem(id);
+        }
+
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
